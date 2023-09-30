92c1743a92438fe4a4417e754e9a7f40e12a0146
Amberichu <4722249+davchoo@users.noreply.github.com>
2023-09-30 03:01:22 -0400

    docs: Comment stages in registerLoginTest

--- src/test/java/homerep/springy/RegistrationTests.java
+++ src/test/java/homerep/springy/RegistrationTests.java
@@ -40,33 +40,35 @@ class RegistrationTests {
 
 	@Test
 	void registerLoginTest() throws Exception {
+		// Register an account
 		this.mvc.perform(post("/api/register")
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(mapper.writeValueAsString(new AccountModel(TEST_EMAIL, TEST_PASSWORD))))
 				.andExpect(status().isOk());
-
+		// An account is created and stored to the database
 		Account account = accountRepository.findByEmail(TEST_EMAIL);
 		assertEquals(TEST_EMAIL, account.getEmail());
 		assertTrue(passwordEncoder.matches(TEST_PASSWORD, account.getPassword()));
 		assertFalse(account.isVerified());
 		assertNotNull(account.getVerificationToken());
 
+		// Invalid verification tokens are rejected
 		this.mvc.perform(get("/api/verify?token={token}", INVALID_TOKEN))
 				.andExpect(status().isBadRequest());
-
+		// No change to the account
 		Account afterFailedVerifyaccount = accountRepository.findByEmail(TEST_EMAIL);
 		assertEquals(account, afterFailedVerifyaccount);
-
+		// Use the correct verification token
 		this.mvc.perform(get("/api/verify?token={token}", account.getVerificationToken()))
 				.andExpect(status().isOk());
-
+		// Verification status is updated
 		Account afterVerifyaccount = accountRepository.findByEmail(TEST_EMAIL);
 		assertTrue(afterVerifyaccount.isVerified());
 		assertNull(afterVerifyaccount.getVerificationToken());
-
+		// Token is invalidated
 		this.mvc.perform(get("/api/verify?token={token}", account.getVerificationToken()))
 				.andExpect(status().isBadRequest());
-
+		// Authenticated after login
 		this.mvc.perform(post("/api/login")
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(mapper.writeValueAsString(new AccountModel(TEST_EMAIL, TEST_PASSWORD))))
