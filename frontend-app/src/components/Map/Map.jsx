657bb5051b5ea8d3f2369e47859affe402b74d37
Lukas DeLoach <lukasdeloach@gmail.com>
2023-10-20 15:28:27 -0400

    fix: env variable fix

--- frontend-app/src/components/Map/Map.jsx
+++ frontend-app/src/components/Map/Map.jsx
@@ -8,12 +8,12 @@ import GoogleMapReact from "google-map-react"
 const Map = ({ places, setCoords, setBounds, setChildClicked, weatherData }) => {
   //const matches = useMediaQuery('(min-width:600px)');
   //const classes = useStyles();
-  const coords = {lat: 0, lng: 0}
+  const coords = {lat: 52, lng: -75}
 
   return (
     <div className='h-[85vh] w-100%'>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyB-Hir-BFLaHrDngWHU5dXi3wA4VfIshs4' }}
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
         defaultCenter={coords}
         center={coords}
         defaultZoom={14}
