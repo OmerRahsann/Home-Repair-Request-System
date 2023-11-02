edb73713d3ed529bca07410dbdc7d9751bf2f34a
Lukas DeLoach <lukasdeloach@gmail.com>
2023-11-02 15:09:27 -0400

    feature: reverse geocoding the addresses as they come in

--- frontend-app/src/components/Map/ProviderMap.jsx
+++ frontend-app/src/components/Map/ProviderMap.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import GoogleMapReact from 'google-map-react'
-import mapStyles from '../../mapStyles'
-import useStyles from './styles.js'
+import React, { useState, useEffect } from 'react';
+import GoogleMapReact from 'google-map-react';
+import mapStyles from '../../mapStyles';
+import useStyles from './styles.js';
 import {
   Paper,
   Typography,
   useMediaQuery,
   CircularProgress,
-} from '@material-ui/core'
-import { useState, useEffect } from 'react'
-import styles from './styles.js'
+} from '@material-ui/core';
+import styles from './styles.js';
 
 const ProviderMap = ({
   coords,
@@ -18,10 +17,47 @@ const ProviderMap = ({
   setBounds,
   onCardClicked,
 }) => {
-  const matches = useMediaQuery('(min-width:600px)')
-  const classes = useStyles()
-  const [loading, setLoading] = useState(true)
-  const [childClicked, setChildClicked] = useState(null)
+  const matches = useMediaQuery('(min-width:600px)');
+  const classes = useStyles();
+  const [loading, setLoading] = useState(true);
+  const [childClicked, setChildClicked] = useState(null);
+  const [latLng, setLatLng] = useState(null);
+
+  const reverseGeocode = async (address) => {
+    try {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`,
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        if (data.results.length > 0) {
+          const location = data.results[0].geometry.location;
+          return location;
+        } else {
+          console.error('No results found for the given address.');
+          return null;
+        }
+      } else {
+        console.error('Geocoding request failed:', response.statusText);
+        return null;
+      }
+    } catch (error) {
+      console.error('Error during geocoding request:', error);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    // Iterate through the requests to reverse geocode the addresses and update the state
+    const geocodePromises = requests.map((request) => reverseGeocode(request.address));
+
+    Promise.all(geocodePromises).then((locations) => {
+      // Update the state with the locations
+      setLatLng(locations);
+      setLoading(false);
+    });
+  }, [requests]);
 
   return (
     <div className={classes.mapContainer}>
@@ -37,17 +73,16 @@ const ProviderMap = ({
         }}
         margin={[50, 50, 50, 50]}
         onChange={(e) => {
-          console.log(e)
-          setCoords({ lat: e.center.lat, lng: e.center.lng })
-          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw })
+          setCoords({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
         }}
         onGoogleApiLoaded={() => setLoading(false)}
       >
-        {requests.length != 0 && requests.map((request, i) => (
+        {requests.length !== 0 && requests.map((request, i) => (
           <div
             className={classes.markerContainer}
-            lat={Number(request.latitude)}
-            lng={Number(request.longitude)}
+            lat={latLng && latLng[i] ? latLng[i].lat : Number(request.latitude)}
+            lng={latLng && latLng[i] ? latLng[i].lng : Number(request.longitude)}
             key={i}
             onClick={() => onCardClicked(i)}
           >
@@ -57,8 +92,7 @@ const ProviderMap = ({
                 variant="subtitle2"
                 gutterBottom
               >
-                {' '}
-                {request.title}{' '}
+                {request.title}
               </Typography>
               <img
                 className={classes.pointer}
@@ -73,7 +107,7 @@ const ProviderMap = ({
         ))}
       </GoogleMapReact>
     </div>
-  )
-}
+  );
+};
 
-export default ProviderMap
+export default ProviderMap;
