52fc1842249b7cce2e29cc3d64cef1420596aeb9
Lukas DeLoach <lukasdeloach@gmail.com>
2023-12-11 16:27:22 -0500

    fix: browser auto complete fix with address

--- frontend-app/src/pages/Authentication/ProviderSignUp.jsx
+++ frontend-app/src/pages/Authentication/ProviderSignUp.jsx
@@ -177,7 +177,7 @@ function ProviderSignUp() {
               <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
                 <div>
                   <input
-                    placeholder="Business Address"
+                    placeholder="Business Location"
                     required
                     autoComplete="new-password"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 "
