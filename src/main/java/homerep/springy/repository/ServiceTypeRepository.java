d300c9d5ce267a6d8ac03c7f5d1f7cfb970054e7
Amberichu <4722249+davchoo@users.noreply.github.com>
2023-12-14 16:48:26 -0500

    feature: Manual query for service types
    We don't care about the entity or its id

--- src/main/java/homerep/springy/repository/ServiceTypeRepository.java
+++ src/main/java/homerep/springy/repository/ServiceTypeRepository.java
@@ -3,15 +3,17 @@ package homerep.springy.repository;
 import homerep.springy.entity.ServiceType;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ServiceTypeRepository extends JpaRepository<ServiceType, Integer> {
+
+    @Query("select s.serviceType from ServiceType s")
+    List<String> _findAllServices(Sort sort);
+
     default List<String> findAllServices() {
-        List<String> services = findAll(Sort.by("serviceType").ascending())
-                .stream()
-                .map(ServiceType::getServiceType)
-                .toList();
+        List<String> services = _findAllServices(Sort.by("serviceType").ascending());
         if (services.isEmpty()) {
             // We're in a dev environment and services is empty.
             // Return some random services.
