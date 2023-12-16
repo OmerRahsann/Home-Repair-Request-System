8d3f613a504a1fbd32b301cddc7966f132d0942f
Lukas DeLoach <lukasdeloach@gmail.com>
2023-12-16 12:54:38 -0500

    fix: fix send email button

--- frontend-app/src/components/ServiceProviderHome/RequestDetailsProvider.jsx
+++ frontend-app/src/components/ServiceProviderHome/RequestDetailsProvider.jsx
@@ -104,7 +104,7 @@ function RequestDetailsProvider({ request }) {
                       </button>
                       <break className="pt-2 md:pl-2 "></break>
                       <button className="text-white bg-custom-maroon hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                        <a href={`mailto:${email}`}>Send Email</a>
+                        <a href={`mailto:${email.email}`}>Send Email</a>
                       </button>
                     </div>
                   )}
